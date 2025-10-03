576840d1cfed6f4326e645abf0f106439b942d3b
Mars Burgtorf <burgtorf2@gmail.com>
2025-10-03 15:58:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2102177354-e6f15",
+  "appId": "1:931254312491:web:8095343bb6146191ff946e",
+  "apiKey": "AIzaSyDYkzuu2zqIUje9XT3a1LXIdfS0ykWL92o",
+  "authDomain": "studio-2102177354-e6f15.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "931254312491"
+};
